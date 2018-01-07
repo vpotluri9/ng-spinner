8ff19ecc0cbf551213532aa27b1e88253ca56480
Venkatesh Potluri <vpotluri07@gmail..com>
2018-01-07 02:58:04 -0600

    first commit

---
+++ src/lib/src/component/lib.component.ts
@@ -0,0 +1,12 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'ng-spinner',
+  templateUrl: './lib.component.html',
+  styleUrls: ['./lib.component.css']
+})
+export class LibComponent {
+
+  @Input() display = false;
+
+}
